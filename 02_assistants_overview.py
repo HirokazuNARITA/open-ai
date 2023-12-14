d3c8cf5194337397d3d095efb8e9ab3f2ffb1d4f
narita <narita@sansou.co.jp>
2023-12-14 18:45:57 +0900

    コメントを追加

--- 02_assistants_overview.py
+++ 02_assistants_overview.py
@@ -3,9 +3,11 @@ import traceback
 from openai import OpenAI
 from common.helper import retrieve_runs
 
+# OpenAIクライアントの初期化
 client = OpenAI()
 
 try:
+    # 数学の家庭教師アシスタントを作成
     assistant = client.beta.assistants.create(
         name="数学の家庭教師",
         description="あなたは数学の家庭教師です。コードを書いて実行し、数学の質問に答えてください。",
@@ -15,10 +17,12 @@ try:
     print("-----assistantの生成-------")
     print(assistant)
 
+    # スレッドの作成
     thread = client.beta.threads.create()
     print("------threadの生成---------")
     print(thread)
 
+    # ユーザーからのメッセージをスレッドに投稿
     message = client.beta.threads.messages.create(
         thread_id=thread.id,
         role="user",
@@ -27,19 +31,21 @@ try:
     print("-------messageの生成---------")
     print(message)
 
+    # アシスタントに指示を出すためのrunを作成
     run = client.beta.threads.runs.create(
         thread_id=thread.id,
         assistant_id=assistant.id,
         instructions="ユーザー名を 成田 浩和 としてください。このユーザーはプレミアムアカウントを持っています。",
     )
-
     print("------runの生成--------")
     print(run)
 
-    run = retrieve_runs(client=client, thread_id=thread.id, run_id=run.id)
-    print("-----runの回収-----")
-    print(run)
+    # runの詳細を取得
+    run_details = retrieve_runs(client=client, thread_id=thread.id, run_id=run.id)
+    print("-----runの詳細-----")
+    print(run_details)
 
+    # スレッド内のメッセージを取得
     messages = client.beta.threads.messages.list(thread_id=thread.id)
     print("--------messagesのリストアップ-------")
     print(messages)
@@ -55,17 +61,19 @@ try:
         else "回答が見つかりませんでした。"
     )
 
-
 except Exception as e:
+    # 予期せぬエラーのキャッチとトレースバックの出力
     print("予期せぬエラーが発生しました:", {e})
     print(traceback.print_exc())
 
 finally:
-    # 後片付け
+    # 後片付け処理
+    # スレッドの削除
     if thread:
         result_del = client.beta.threads.delete(thread.id)
         print(result_del)
 
+    # アシスタントの削除
     if assistant:
         result_del_assistant = client.beta.assistants.delete(assistant.id)
         print(result_del_assistant)
