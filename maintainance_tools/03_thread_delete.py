5c413e8dbc1b0fd5b0a419ebabb0379e6132a85a
narita <narita@sansou.co.jp>
2024-05-07 17:32:58 +0900

    メンテナンスツールにコメントを追加

--- maintainance_tools/03_thread_delete.py
+++ maintainance_tools/03_thread_delete.py
@@ -10,3 +10,10 @@ for id in thead_ids:
     client.beta.threads.delete(thread_id=id)
 
 print("threads deleted")
+
+
+# OpenAIのプラットフォーム＞Thread画面でChromeのDeveloperツールのコンソールを起動して以下のコードを実行すると
+# 画面上のthread_idを取得できる。
+# const elements = Array.from(document.querySelectorAll('.thread-list-subheading-id'));
+# const texts = elements.map(element => element.innerText);
+# console.log(texts);
