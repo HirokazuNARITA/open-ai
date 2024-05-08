fd1eb6c66137f09f683d34e732e86721daee45ee
narita <narita@sansou.co.jp>
2024-05-08 16:39:15 +0900

    file_searchについてのサンプルを更新

--- 04_file_search.py
+++ 04_file_search.py
@@ -15,33 +15,6 @@ ASSISTANT_ID = os.getenv("ASSISTANT_ID")
 THREAD_ID = os.getenv("THREAD_ID")
 
 
-# class EventHandler(AssistantEventHandler):
-#     @override
-#     def on_text_created(self, text) -> None:
-#         print(f"\nassistant > ", end="", flush=True)
-
-#     @override
-#     def on_tool_call_created(self, tool_call):
-#         print(f"\nassistant > {tool_call.type}\n", flush=True)
-
-#     @override
-#     def on_message_done(self, message) -> None:
-#         # print a citation to the file searched
-#         message_content = message.content[0].text
-#         annotations = message_content.annotations
-#         citations = []
-#         for index, annotation in enumerate(annotations):
-#             message_content.value = message_content.value.replace(
-#                 annotation.text, f"[{index}]"
-#             )
-#             if file_citation := getattr(annotation, "file_citation", None):
-#                 cited_file = client.files.retrieve(file_citation.file_id)
-#                 citations.append(f"[{index}] {cited_file.filename}")
-
-#         print(message_content.value)
-#         print("\n".join(citations))
-
-
 def response_chatbot(assistant, thread, user_input):
     message = client.beta.threads.messages.create(
         thread_id=thread.id, role="user", content=user_input
@@ -50,7 +23,6 @@ def response_chatbot(assistant, thread, user_input):
     with client.beta.threads.runs.stream(
         thread_id=thread.id,
         assistant_id=assistant.id,
-        # instructions="Please address the user as Jane Doe. The user has a premium account.",
         event_handler=StreamingEventHandler(assistant_pronpt="チャットボット: "),
     ) as stream:
         stream.until_done()
@@ -62,6 +34,21 @@ def main():
     チャットボットのセッションを開始します。ユーザーの入力に応じて応答を生成し、
     特定のキーワードでセッションを終了します。
     """
+    # インスタンスの削除フラグを設定します。
+    delete_instance = False
+
+    # アシスタント
+    assistant = None
+
+    # 初期ファイル
+    file = None
+
+    # ベクターストア
+    vector_store = None
+
+    # スレッド
+    thread = None
+
     try:
 
         # アシスタントのインスタンスを取得または作成します。
@@ -69,7 +56,7 @@ def main():
             client.beta.assistants.retrieve(ASSISTANT_ID)
             if ASSISTANT_ID
             else client.beta.assistants.create(
-                instructions="あなたはカスタマーサポートのチャットボットです。ナレッジベースを活用して、顧客からの問い合わせに最適な対応をしましょう。ユーザーには日本語で回答してください。",
+                instructions="あなたはユーザーの回答に答える優秀なキュレーターです。ユーザーからの質問に対して、添付された資料やナレッジベースを最優先で参照し回答することを求められています。ユーザーには日本語で回答してください。",
                 model="gpt-4-turbo",
                 tools=[{"type": "file_search"}],
             )
@@ -80,25 +67,26 @@ def main():
 
         # OpenAIにアップロードするファイルを準備する
         # contextLib.ExitStackは、登録されたすべてのコンテキストマネージャ（この場合はファイルオブジェクト）を適切に閉じることを保証します。
-        with contextlib.ExitStack() as stack:
-            # ファイルをアシスタントにアップロードします。
-            file = client.files.create(
-                file=stack.enter_context(open("./sample_files/manual.pdf", "rb")),
-                purpose="assistants",
+        if not ASSISTANT_ID:
+            with contextlib.ExitStack() as stack:
+                # ファイルをアシスタントにアップロードします。
+                file = client.files.create(
+                    file=stack.enter_context(open("./sample_files/manual.pdf", "rb")),
+                    purpose="assistants",
+                )
+
+                file_batch = client.beta.vector_stores.file_batches.create_and_poll(
+                    vector_store_id=vector_store.id, file_ids=[file.id]
+                )
+
+                print(file_batch.status)
+                print(file_batch.file_counts)
+
+            # アシスタントにvector storeを参照させるようにアップデートする
+            assistant = client.beta.assistants.update(
+                assistant_id=assistant.id,
+                tool_resources={"file_search": {"vector_store_ids": [vector_store.id]}},
             )
-
-            file_batch = client.beta.vector_stores.file_batches.create_and_poll(
-                vector_store_id=vector_store.id, file_ids=[file.id]
-            )
-
-            print(file_batch.status)
-            print(file_batch.file_counts)
-
-        # アシスタントにvector storeを参照させるようにアップデートする
-        assistant = client.beta.assistants.update(
-            assistant_id=assistant.id,
-            tool_resources={"file_search": {"vector_store_ids": [vector_store.id]}},
-        )
 
         # スレッドのインスタンスを取得または作成します。
         thread = (
@@ -111,11 +99,21 @@ def main():
         update_env_file(".env", "ASSISTANT_ID", assistant.id)
         update_env_file(".env", "THREAD_ID", thread.id)
 
-        # インスタンスの削除フラグを設定します。
-        delete_instance = False
-
         # チャットボットの開始メッセージを表示します。
         print("チャットボット: こんにちは！何か聞きたいことはありますか？")
+
+        # メッセージから別のファイルを添付する（別のvector_storeが作成され、これも参照できるようになる）
+        # with open("./sample_files/sample_text.txt", "rb") as file:
+        #     message_file = client.files.create(file=file, purpose="assistants")
+        #     message = client.beta.threads.messages.create(
+        #         thread_id=thread.id,
+        #         role="user",
+        #         content="このファイルも参照して回答してください。",
+        #         attachments=[
+        #             {"file_id": message_file.id, "tools": [{"type": "file_search"}]}
+        #         ],
+        #     )
+
         while True:
             # ユーザーからの入力を受け取ります。
             user_input = input("あなた: ")
@@ -143,15 +141,20 @@ def main():
     finally:
         # セッション終了時にインスタンスを削除するためのクリーンアップ処理を行います。
         if delete_instance:
-            if assistant:
-                result_del_assistant = client.beta.assistants.delete(assistant.id)
-                print(result_del_assistant)
             if file:
                 result_del_file = client.files.delete(file_id=file.id)
                 print(result_del_file)
+            if vector_store:
+                result_del_vc = client.beta.vector_stores.delete(
+                    vector_store_id=vector_store.id
+                )
+                print(result_del_vc)
             if thread:
                 result_del_thread = client.beta.threads.delete(thread.id)
                 print(result_del_thread)
+            if assistant:
+                result_del_assistant = client.beta.assistants.delete(assistant.id)
+                print(result_del_assistant)
 
             # 環境変数ファイルをクリアします。
             update_env_file(".env", "ASSISTANT_ID", "")
